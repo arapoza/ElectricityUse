d6450553b8d031ba6a5e72d32e2cff055c3cd3f9
Drew Rapoza <arapoza@deerfield.edu>
2016-01-07 20:43:56 -0500

    add welcome message and first line of output to program

--- src/ElectricityUse.java
+++ src/ElectricityUse.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class ElectricityUse
 {
@@ -5,7 +6,9 @@ public class ElectricityUse
 	public static void main(String[] args)
 	{
 		// TODO Auto-generated method stub
-
+		System.out.println("Tool to calculate your computer's lifetime cost");
+		System.out.println("Enter the total cost of the computer you plan to buy");
+		
 	}
 
 }
