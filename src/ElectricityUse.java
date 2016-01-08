450c27987b32f4e64cf7d3f92452c5efb9c5b98f
Drew Rapoza <arapoza@deerfield.edu>
2016-01-07 20:52:24 -0500

    add first user input of total cost

--- src/ElectricityUse.java
+++ src/ElectricityUse.java
@@ -6,9 +6,11 @@ public class ElectricityUse
 	public static void main(String[] args)
 	{
 		// TODO Auto-generated method stub
-		System.out.println("Tool to calculate your computer's lifetime cost");
-		System.out.println("Enter the total cost of the computer you plan to buy");
 		
+		System.out.println("Tool to calculate your computer's lifetime cost");
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter the total cost of the computer you plan to buy");
+		double cost = scan.nextDouble();
 	}
 
 }
