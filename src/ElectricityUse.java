e92d3f3e5d23a866a143e55aba4b2646ade255af
Drew Rapoza <arapoza@deerfield.edu>
2016-01-07 20:59:26 -0500

    add prompt and input to variable for type of computer

--- src/ElectricityUse.java
+++ src/ElectricityUse.java
@@ -8,9 +8,12 @@ public class ElectricityUse
 		// TODO Auto-generated method stub
 		
 		System.out.println("Tool to calculate your computer's lifetime cost");
+		
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Enter the total cost of the computer you plan to buy");
 		double cost = scan.nextDouble();
+		
+		System.out.println("the cost is" + cost); 
 	}
 
 }
